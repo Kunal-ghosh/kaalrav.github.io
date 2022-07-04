5cfd0512e0db108f350e9b61c4abad620c8af451
Kunal-ghosh <77928225+Kunal-ghosh@users.noreply.github.com>
2022-07-04 11:37:05 +0530

    2.0
    adding different pages

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+// const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+// const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+// const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+// const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
@@ -10,8 +14,8 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
-    if (scroll_position > 250) {
-        header.style.backgroundColor = '#29323c';
+    if (scroll_position > 0) {
+        header.style.backgroundColor = 'transparent';
     } else {
         header.style.backgroundColor = 'transparent';
     }
