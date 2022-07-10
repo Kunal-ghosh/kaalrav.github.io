196cd4a92b641e73e1881721b555e2c1a2e16ab1
Kunal-ghosh <77928225+Kunal-ghosh@users.noreply.github.com>
2022-07-10 18:33:26 +0530

    3.1
    social icons added

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
-    if (scroll_position > 0) {
-        header.style.backgroundColor = 'transparent';
+    if (scroll_position > 450) {
+        header.style.backgroundColor = '#000000';
     } else {
         header.style.backgroundColor = 'transparent';
     }
