34ae4210ef36485baf1f1cc52577dcdfa87dc17b
Kunal-ghosh <77928225+Kunal-ghosh@users.noreply.github.com>
2022-09-04 22:29:34 +0530

    7.0

---
+++ chat-responses.js
@@ -0,0 +1,51 @@
+function getBotResponse(input) {
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } 
+    
+    else if (input == "how are you") 
+    {
+        return "i am fine! how are you";
+    } 
+
+    else if (input == "I am having problem with registration") 
+    {
+        return "Go to the contact section and mail or dm in Instagram to our over all coordinators";
+    } 
+
+    else if (input == "where is Smit located") 
+    {
+        return "Majitar, Rangpo, Sikkim 737136";
+    } 
+
+
+    else if (input == "location") 
+    {
+        return "Majitar, Rangpo, Sikkim 737136";
+    } 
+
+    else if (input == "registration") 
+    {
+        return "Go to the contact section and mail or dm in Instagram to our over all coordinators";
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    else {
+        return "Try asking something else!";
+    }
+}
